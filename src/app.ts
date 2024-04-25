078b2c4dd12f5d3183990da6040c4531945ace73
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2024-04-25 22:01:36 +0200

    feature: created authGuardMiddleware component

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { CourseViewModel } from "./models/CourseViewModel";
@@ -11,6 +11,20 @@ import { addressesRouter } from "./routes/addresses-router";
 
 export const app = express();
 
+const authGuardMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.query.token === "123") {
+    next();
+  } else {
+    res.send(401);
+  }
+};
+
+app.use(authGuardMiddleware);
+
 const corsMiddleware = cors();
 app.use(corsMiddleware);
 
@@ -22,5 +36,17 @@ app.use("/courses", getCoursesRouter(db));
 app.use("/__test__", getTestsRouter(db));
 app.use("/interesting", getInterestingRouter(db));
 
-app.use("/products", productsRouter);
+//app.use("/products", productsRouter);
 app.use("/addresses", addressesRouter);
+
+app.get(
+  "/products",
+  (req: Request, res: Response, next: NextFunction) => {
+    //req.blabla = "hello"
+    next();
+  },
+  (req: Request, res: Response) => {
+    //const blabla = req.blabla
+    res.send({ value: "blabla" });
+  }
+);
